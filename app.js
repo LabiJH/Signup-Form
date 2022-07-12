cd1551d9b0c7bf12560b626269bc193ce43cbba1
Labinot Jaha <labinot@jaha.dev>
2022-07-12 23:08:08 +0200

    Added valid + invalid JS Checks

--- app.js
+++ app.js
@@ -8,9 +8,26 @@ function passcheck(){
     {
         submitButton.disabled = true;
     }
-    
+    else{
+        submitButton.disabled = false;
+    }
 }
 
 
 password.addEventListener("onkeyup", passcheck());
 passwordConfirm.addEventListener("onkeyup", passcheck());
+/*
+let fname = document.querySelector("#fname");
+
+fname.addEventListener("keyup", ()=>{
+    if(fname.checkValidity() == true)
+    {
+        document.querySelector(".validCheck").innerHTML = "valid";
+        document.querySelector(".validCheck").style.display = "block";
+        document.querySelector(".validCheck").backgroundColor = "lime";
+    }
+    else{
+        document.querySelector(".validCheck").innerHTML = "Invalid";
+        ocument.querySelector(".validCheck").backgroundColor = "red";
+    }
+}) */
